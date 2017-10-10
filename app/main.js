5b7f9e4a99bbf73e378632f598d144f95309a97f
Heath Williams <awilli01@amgen.com>
2017-10-10 11:14:22 -0400

    form and data added

--- app/main.js
+++ app/main.js
@@ -10,37 +10,118 @@ var contactList = new Vue( {
 	el : '#contactList',
 
 	data : {
+		attemptingSave : false,
+
 		newName : '',
-		newPhoneNumber : '',
-		contacts : [
+		newPhone : '',
+		newEmail : '',
+
+		contactList : [ 
 			{
 				id : 1,
 				name : 'Albus Dumbledore',
-				phoneNumber : '859-123-4567'
+				phone : '859-123-4567',
+				email : 'headmaster@hogwarts.edu'
 			},
 			{
 				id : 2,
 				name : 'Minerva McGonagall',
-				phoneNumber : '606-234-9876'
+				phone : '606-234-9876',
+				email : 'quiddich.is.best.g@hogwarts.edu'
 			},
 			{
 				id : 3,
 				name : 'Severus Snape',
-				phoneNumber : '502-444-1010'
+				phone : '502-444-1010',
+				email : 'potions.maybe.dark.arts@hogwarts.edu'
 			},
-		],
-		nextContactId : 4
+		], // Stored data, unsorted
+		
 	},
 
+	computed : { // Computed fields
+		contacts : computedContacts,
+		missingName : missingName,
+		badPhone : badPhone,
+		badEmail : badEmail
+	},
+
+	beforeMount : beforeMount,
+
 	methods : {
-		addContact : addContact,
+		alphabetizeContacts : alphabetizeContacts,
 		removeContact : removeContact,
 		findContact : findContact,
+		validateForm : validateForm
+
+		/*
+		addContact : addContact,
+		
 		validatePhoneNumber : validatePhoneNumber
-	}
+		*/
+	},
 
 });
 
+function computedContacts() {
+	return this.contactList.sort( this.alphabetizeContacts );
+}
+
+function missingName() {
+	return this.newName === '';
+}
+
+function badEmail() {
+	// return ( this.newEmail.indexOf( '@' ) !== 0 );
+
+	var pattern = /@/;
+	return ! pattern.test( this.newEmail );
+}
+
+function badPhone() {
+	var pattern = /^[1-9]\d{2}-\d{3}-\d{4}$/;
+	return ! pattern.test( this.newPhone );
+}
+
+function beforeMount() {
+
+}
+
+function alphabetizeContacts( a, b ) {
+	var aName = a.name.toLowerCase();
+	var bName = b.name.toLowerCase();
+
+	if ( aName < bName ) {
+		return -1;
+	}
+	else if ( aName > bName ) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+function removeContact( id ) {
+	var contactIndex = this.findContact( id );
+	this.contacts.splice( contactIndex, 1 );
+}
+
+function findContact( id ) {
+	return this.contacts.findIndex( 
+		function( contact ) { 
+			return ( id === contact.id ); 
+		} 
+	);
+}
+
+function validateForm() {
+	this.attemptingSave = true;
+}
+
+
+
+/*
 function validatePhoneNumber() {
 	var phoneRe = /^[0-9\-\+\s\(\)]*$/;
 
@@ -83,3 +164,4 @@ function findContact( id ) {
 		} 
 	);
 }
+*/
